08e66efbd52cbf320fd86b17ae20608a6ccc5a90
GrigorevValentin <radiustex@gmail.com>
2024-05-20 20:42:57 +0300

    2.1.4 готовая

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,31 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Egg6 {
+    private final Needle7 needle;
+
+    public Egg6(Needle7 needle) {
+        this.needle = needle;
+    }
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needle.toString();
     }
 }
+/*
+private final Duck5 duck;
+
+    @Autowired
+    public Rabbit4(Duck5 duck) {
+        this.duck = duck;
+    }
+
+    @Override
+    public String toString() {
+        return ", в зайце утка " + duck.toString();
+    }
+}
+ */
