de970f4a8d0735e4f0ee6f0da3589d3aae2d05cb
GrigorevValentin <radiustex@gmail.com>
2024-05-20 18:06:37 +0300

    2.1.3 Готовая

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -4,7 +4,17 @@ import app.config.AppConfig;
 import app.model.AnimalsCage;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+/*
+Создайте бин Dog, унаследуйте его от Animal. Создайте на основе этого класса компонент.
 
+Запустите приложение и проверьте, что было выброшено исключение NoUniqueBeanDefinitionException. Это произошло из-за того, что существует 2 бина с типом Animal.
+
+Прочитайте о связывании бинов по имени и свяжите AnimalCage c бином Dog через абстрактный класс Animal.
+
+На основе класса Timer создайте бин. Свяжите с AnimalCage. Проверьте, что при выполнении метода main время, которое пишет таймер, одно и тоже.
+
+Раскомментировать тест из заготовки и проверить своё решение.
+ */
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,33 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
+    private Animal firstAnimal;
+    private Animal secondAnimal;
+    public Timer time;
+
     @Autowired
-    private Animal animal;
+    public AnimalsCage(@Qualifier("cat") Animal firstAnimal, @Qualifier("dog") Animal secondAnimal, Timer time) {
+        this.firstAnimal = firstAnimal;
+        this.secondAnimal = secondAnimal;
+        this.time = time;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
-        System.out.println(animal.toString());
+        System.out.println(firstAnimal.toString()+ ", "+secondAnimal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(time.getTime());
         System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return time;
+
     }
 }
